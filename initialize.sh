541ed0a26300f7d2649937e83c5b42da0d167bb7
Kevin Yokley <kyokley2@gmail.com>
2015-07-20 15:22:31 -0500

    Running commands as user

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-apt-get update
-apt-get install aptitude -y
-aptitude install git -y
+sudo apt-get update
+sudo apt-get install aptitude -y
+sudo aptitude safe-upgrade -y
+sudo aptitude install git -y
 
+sudo adduser yokley
+sudo adduser yokley adm
+sudo adduser yokley sudo
+
+exec sudo -i -u yokley /bin/bash - << eof
 git clone https://github.com/kyokley/vimRepo.git -b nologin ~/.vim
 git clone https://github.com/kyokley/dotfiles.git -b nologin ~/dotfiles
 
@@ -12,3 +18,4 @@ bash install.sh
 
 cd ~/dotfiles
 bash install.sh
+eof
