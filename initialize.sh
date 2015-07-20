90e0901c8a70f6cc84bf0c682c8faa21fadf95a9
Kevin Yokley <kyokley2@gmail.com>
2015-07-19 20:11:29 -0500

    Executing script as other user

--- initialize.sh
+++ initialize.sh
@@ -4,11 +4,19 @@ apt-get update
 apt-get install aptitude -y
 aptitude install git -y
 
-git clone https://github.com/kyokley/vimRepo.git -b nologin ~/.vim
-git clone https://github.com/kyokley/dotfiles.git -b nologin ~/dotfiles
+adduser yokley
+adduser yokley adm
+adduser yokley sudo
+
+exec sudo -i -u yokley /bin/bash - << eof
+
+git clone https://github.com/kyokley/vimRepo.git -b nologin --recursive ~/.vim
+git clone https://github.com/kyokley/dotfiles.git -b nologin --recursive ~/dotfiles
 
 cd ~/.vim
 bash install.sh
 
 cd ~/dotfiles
 bash install.sh
+
+eof
