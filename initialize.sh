d44bc951da7a924b9ff49296175ab8734d3608c7
Kevin Yokley <kyokley2@gmail.com>
2015-07-21 08:40:55 -0500

    Separating vim and dotfile setups

--- initialize.sh
+++ initialize.sh
@@ -11,10 +11,14 @@ sudo adduser yokley sudo
 
 exec sudo -i -u yokley /bin/bash - << eof
 git clone https://github.com/kyokley/vimRepo.git -b nologin ~/.vim
-git clone https://github.com/kyokley/dotfiles.git -b nologin ~/dotfiles
 
 cd ~/.vim
 bash install.sh
+
+eof
+
+exec sudo -i -u yokley /bin/bash - << eof
+git clone https://github.com/kyokley/dotfiles.git -b nologin ~/dotfiles
 
 cd ~/dotfiles
 bash install.sh
